9b5ebad8a9bfe6e09f573ccfa983c9467974e52a
PabloMira1 <pablomirab03@gmail.com>
2024-11-05 10:13:03 -0500

    Railway 9

--- src/main/java/com/example/demo/WebService/WebConfig.java
+++ src/main/java/com/example/demo/WebService/WebConfig.java
@@ -1,27 +1,33 @@
 package com.example.demo.WebService;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .requiresChannel(channel -> channel
-                        .anyRequest().requiresSecure() // Redirige HTTP a HTTPS
-                )
-                .cors(cors -> cors.disable()) // Configura CORS usando Lambda DSL
-                .csrf(csrf -> csrf.disable()) // Desactiva CSRF si es necesario
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Permite todas las solicitudes (ajusta según tus requisitos de
-                                                  // seguridad)
-                );
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of("https://mi-proyecto-neon.vercel.app")); 
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+                configuration.setAllowCredentials(true);
+                return configuration;
+            }))
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()  // Ajusta según tus necesidades
+            );
 
         return http.build();
     }
